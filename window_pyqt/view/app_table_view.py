0a3a583bf3701cb0b36f3d6f5f7147eafe3cb249
hyd <1357644645@qq.com>
2025-01-12 22:54:46 +0800

    app界面完成

--- window_pyqt/view/app_table_view.py
+++ window_pyqt/view/app_table_view.py
@@ -8,7 +8,7 @@ from typing import Any
 
 from PyQt6 import QtWidgets, QtCore
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QHBoxLayout, QTableWidgetItem, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QHBoxLayout, QTableWidgetItem, QWidget, QVBoxLayout, QTableWidget, QHeaderView
 from qfluentwidgets import TableWidget, PrimaryPushButton, ComboBox, BodyLabel, MessageBoxBase, LineEdit
 
 from database_service.model.app_model import App
@@ -72,9 +72,7 @@ class AppTableView(Widget):
 
         # 清空现有数据
         self.tableView.clearContents()
-        # start_ = 15 * (page_number - 1)
-        # end_ = start_ + 15
-        # songInfos = songInfos[start_:end_]
+
         for i, obj_ in enumerate(app_list_obj):
             self.tableView.setItem(i, 0, QTableWidgetItem(obj_.app_name))
             self.tableView.setItem(i, 1, QTableWidgetItem(obj_.package_name))
@@ -82,28 +80,32 @@ class AppTableView(Widget):
             self.tableView.setItem(i, 3, QTableWidgetItem(obj_.download_link))
             self.tableView.setItem(i, 4, QTableWidgetItem(obj_.download_method))
 
+            detail_btn = QtWidgets.QPushButton("详细")
+            detail_btn.setStyleSheet("background-color: green; color: black;")
+            detail_btn.clicked.connect(lambda checked, current=obj_: self.show_detail_app_dialog(app_obj=current))
             delete_btn = QtWidgets.QPushButton("删除")
-            delete_btn.setStyleSheet("background:red;color:white;")
-            delete_btn.setFixedSize(70, 20)
+            delete_btn.setStyleSheet("background-color: red; color: black;")
             delete_btn.clicked.connect(lambda checked, current=obj_: self.delete_app(current.id))
 
             # 创建一个容器和布局
             widget = QtWidgets.QWidget()
-            layout = QtWidgets.QVBoxLayout(widget)
+            layout = QtWidgets.QHBoxLayout(widget)
+            layout.setSpacing(5)
             layout.setContentsMargins(0, 0, 0, 0)
 
             # 添加上下左右的弹性空白并居中按钮
-            layout.addStretch()  # 顶部弹性
-            layout.addWidget(delete_btn, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)  # 居中按钮
-            layout.addStretch()  # 底部弹性
+            layout.addStretch()  # 左边弹性
+            layout.addWidget(detail_btn)  # 居中按钮
+            layout.addWidget(delete_btn)  # 居中按钮
+            layout.addStretch()  # 右边弹性
 
             self.tableView.setCellWidget(i, 5, widget)
 
-
+        # 设置表格不可编辑
+        self.tableView.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
         self.tableView.resizeColumnsToContents()
-        # self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
-        # self.tableView.setSortingEnabled(True)
-
+        self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.tableView.setSortingEnabled(True)
         self.setStyleSheet("Demo{background: rgb(255, 255, 255)} ")
 
     def delete_app(self, app_id):
@@ -126,6 +128,13 @@ class AppTableView(Widget):
         else:
             MessageWidget.error_message(self, content="添加失败")
         self.add_app()
+
+    def show_detail_app_dialog(self, app_obj):
+        w = DetailAppDialog(self, app_obj=app_obj)
+        if w.exec():
+            pass
+        else:
+            pass
 
     def add_app(self):
         current_page = self.paging_widget.page_number_
@@ -183,7 +192,71 @@ class AddAppDialog(MessageBoxBase):
 
         self.widget.setMinimumWidth(500)
 
+
+class DetailAppDialog(MessageBoxBase):
+    """ Custom message box """
+
+    def __init__(self, parent=None, app_obj: App = None):
+        super().__init__(parent)
+        # self.resize(1000, 600)
+        vbox1 = QVBoxLayout()
+        vbox2 = QVBoxLayout()
+        hbox1 = QHBoxLayout()
+
+        self.app_name_label = BodyLabel('app名称: ', self)
+        self.app_package_label = BodyLabel('app包名: ', self)
+        self.app_version_label = BodyLabel('app版本: ', self)
+        self.download_url_label = BodyLabel('下载短链接: ', self)
+        self.download_method_label = BodyLabel('下载方式: ', self)
+
+        vbox1.addWidget(self.app_name_label)
+        vbox1.addWidget(self.app_package_label)
+        vbox1.addWidget(self.app_version_label)
+        vbox1.addWidget(self.download_url_label)
+        vbox1.addWidget(self.download_method_label)
+
+        self.app_name_edit = LineEdit(self)
+        self.app_package_edit = LineEdit(self)
+        self.app_version_edit = LineEdit(self)
+        self.download_url_edit = LineEdit(self)
+        self.download_method_edit = LineEdit(self)
+
+        self.app_name_edit.setText(app_obj.app_name)
+        self.app_package_edit.setText(app_obj.package_name)
+        self.app_version_edit.setText(app_obj.version)
+        self.download_url_edit.setText(app_obj.download_link)
+        self.download_method_edit.setText(app_obj.download_method)
+
+        self.app_name_edit.setReadOnly(True)
+        self.app_package_edit.setReadOnly(True)
+        self.app_version_edit.setReadOnly(True)
+        self.download_url_edit.setReadOnly(True)
+        self.download_method_edit.setReadOnly(True)
+
+        vbox2.addWidget(self.app_name_edit)
+        vbox2.addWidget(self.app_package_edit)
+        vbox2.addWidget(self.app_version_edit)
+        vbox2.addWidget(self.download_url_edit)
+        vbox2.addWidget(self.download_method_edit)
+
+
+
+        hbox1.addLayout(vbox1)
+        hbox1.addLayout(vbox2)
+
+
+        self.viewLayout.addLayout(hbox1)
+
+        # change the text of button
+        self.yesButton.setText('确定')
+        self.yesButton.disconnect()
+        self.yesButton.clicked.connect(self.validate_detail)
+        self.cancelButton.setText('关闭')
+
+        self.widget.setMinimumWidth(500)
+
     def validate(self):
+        """重写dialog框的yes验证程序"""
         app = App(
             app_name=self.app_name_edit.text().strip(),
             package_name=self.app_package_edit.text().strip(),
@@ -195,7 +268,8 @@ class AddAppDialog(MessageBoxBase):
         # MessageWidget.success_message(self, f"添加 {self.app_name_edit.text().strip()}成功")
         return True
 
-
+    def validate_detail(self):
+        self.accept()
 
 
 class RadioButtonWidget(QWidget):
